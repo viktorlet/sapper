97c8ead260a660c04b5d438fafe9cb54d49b95ee
viktorlet <144104965+viktorlet@users.noreply.github.com>
2023-09-09 12:48:52 +0300

    Добавлены флаги и автоматическое открывание клеток
    Добавлена возможность отмечать клетку флагом.
    Добавлена функция __check_number в класс GamePole. Открывает соседние клетки, если число мин = 0.

--- sapper.py
+++ sapper.py
@@ -9,6 +9,7 @@ class Cell:
         self.is_mine = False
         self.number = 0
         self.is_open = False
+        self.flag = False
 
     @classmethod
     def __check_bool(cls, value):
@@ -64,11 +65,13 @@ class GamePole:
         return self.__pole_cells
 
     def init_pole(self):
+        """Создает поле для игры"""
         self.__init__()
         self.__place_manes()
         self.__count_mines()
 
     def __place_manes(self):
+        """Расставляет бомбы на поле"""
         mines = self.total_mines
         while mines:
             i, j = randint(0, self.n - 1), randint(0, self.m - 1)
@@ -80,11 +83,13 @@ class GamePole:
                 continue
 
     def __count_mines(self):
+        """Расставляет цифры на поле"""
         for i in range(self.n):
             for j in range(self.m):
                 self.pole[i][j].number = self.__check_mines(i, j)
 
     def __check_mines(self, i, j):
+        """Считает число мин вокруг одной клетки"""
         around_mines = 0
         for n in range(i - 1, i + 2):
             for m in range(j - 1, j + 2):
@@ -96,15 +101,35 @@ class GamePole:
         return around_mines
 
     def open_cell(self, i, j):
+        """Открывает клетку  на поле"""
         if not 0 <= i <= self.n - 1 or not 0 <= j <= self.m - 1:
             raise IndexError('некорректные индексы i, j клетки игрового поля')
         self.pole[i][j].is_open = True
+        if self.pole[i][j].number == 0:
+            self.__check_number(i, j)
+
+    def __check_number(self, i, j):
+        """Проверяет число в клетке.
+        Если '0', то открывает все соседние клетки"""
+        for n in range(i - 1, i + 2):
+            for m in range(j - 1, j + 2):
+                if (n != i or m != j) and n >= 0 and m >= 0:
+                    try:
+                        if not self.pole[n][m].is_open:
+                            self.pole[n][m].is_open = True
+                            if self.pole[n][m].number == 0:
+                                self.__check_number(n, m)
+                    except IndexError:
+                        continue
 
     def show_pole(self):
+        """Выводит поле в терминал"""
         for i in range(self.n):
             for j in range(self.m):
                 cell = self.pole[i][j]
-                if not cell.is_open:
+                if cell.flag:
+                    point = emojize(':triangular_flag:')
+                elif not cell.is_open:
                     point = emojize(':brown_square:')
                 elif cell.is_mine:
                     point = emojize(':bomb:')
@@ -122,26 +147,42 @@ s.init_pole()
 
 
 def enter_coordinates():
+    """Запрашивает координаты клетки у пользователя"""
     try:
-        i, j = map(int, input('Введите координаты клетки: ').split())
-        return i, j
-    except:
+        i, *j = input('Введите координаты клетки: ').split()
+        if not all(1 <= x <= 9 for x in (int(i), int(j[0]))):
+            raise TypeError('Некорректные координаты клетки')
+        if len(j) == 2:  # Отмечает клетку флагом
+            if j[1] == 'o':
+                s.pole[int(i) - 1][int(j[0]) - 1].flag = True
+            else:
+                raise TypeError('Некорректный ввод')
+        return int(i), int(j[0])
+    except Exception as e:
+        print(e)
         return enter_coordinates()
 
 
-counter = 0
-while True:
-    s.show_pole()
-    i, j = enter_coordinates()
-    cell = s.pole[i - 1][j - 1]
-    if not cell.is_open:
-        s.open_cell(i - 1, j - 1)
-        counter += 1
-    if cell.is_mine:
+def game():
+    """Запускает игру"""
+    counter = 0
+    while True:
         s.show_pole()
-        print('Вы проиграли')
-        break
-    if counter == 71:
-        print('Вы победили')
-        s.show_pole()
-        break
+        i, j = enter_coordinates()
+        cell = s.pole[i - 1][j - 1]
+        if cell.flag:
+            continue
+        if not cell.is_open and not cell.flag:
+            s.open_cell(i - 1, j - 1)
+            counter += 1
+        if cell.is_mine and not cell.flag:
+            s.show_pole()
+            print('Вы проиграли')
+            break
+        if counter == 71:
+            print('Вы победили')
+            s.show_pole()
+            break
+
+
+game()
